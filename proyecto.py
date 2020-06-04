d8a82bb194056368a8eff32b96cc354449b391df
Vahom <66404151+Vahom@users.noreply.github.com>
2020-06-04 08:12:50 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,11 +1,11 @@
 a= input ('escriba aqui la cadena de ADN')
-dic_aminos={GGG:Gly,GGA:Gly,GGU:Gly,GGC:Gly,GAG:Glu,GAA:Glu,GAU:Asp,GAC:Asp,GUG:Val,GUA:Val,GUU:Val,GUC:Val,GCG:Ala,GCA:Ala,GCU:Ala,GCC:Ala,
-AGG:Arg,AGA:Arg,AGU:Ser,AGC:Ser,AAG:Lys,AAA:Lys,AAU:Asn,AAC:Asn,AUG:Met,AUA:Iso,AUU:Iso,AUC:Iso,ACG:Thr,ACA:Thr,ACU:Thr,ACC:Thr,
-UGG:Try,UGA:STOP,UGU:Cys,UGC:Cys,UAG:STOP,UAA:STOP,UAU:Tyr,UAC:Tyr,UUG:UUA:UUU:UUC:UCG:UCA:UCU:UCC:
-CGG:CGA:CGU:CGC:CAG:CAA:CAU:CAC:CUG:CUA:CUU:CUC:CCG:CCA:CCU:CCC}
+dic_aminos={'GGG':'Gly','GGA':'Gly','GGU':'Gly','GGC':'Gly','GAG':'Glu','GAA':'Glu','GAU':'Asp','GAC':'Asp','GUG':'Val','GUA':'Val','GUU':'Val','GUC':'Val','GCG':'Ala','GCA':'Ala','GCU':'Ala','GCC':'Ala',
+'AGG':'Arg','AGA':'Arg','AGU':'Ser','AGC':'Ser','AAG':'Lys','AAA':'Lys','AAU':'Asn','AAC':'Asn','AUG':'Met','AUA':'Iso','AUU':'Iso','AUC':'Iso','ACG':'Thr','ACA':'Thr','ACU':'Thr','ACC':'Thr',
+'UGG':'Try','UGA':'STOP','UGU':'Cys','UGC':'Cys','UAG':'STOP','UAA':'STOP','UAU':'Tyr','UAC':'Tyr','UUG':'Leu','UUA':'Leu','UUU':'Phe','UUC':'Phe','UCG':'Ser','UCA':'Ser','UCU':'Ser','UCC':'Ser',
+'CGG':'Arg','CGA':'Arg','CGU':'Arg','CGC':'Arg','CAG':'Gln','CAA':'Gln','CAU':'His','CAC':'His','CUG':'Leu','CUA':'Leu','CUU':'Leu','CUC':'Leu','CCG':'Pro','CCA':'Pro','CCU':'Pro','CCC':'Pro'}
 a=str(a)
 count=0
-print(a)
+amino=None
 for i in a:
     #print (i)
     while count<=3:
