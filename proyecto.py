80107940953aab30466dadbfac805f9e6017019b
Vahom <66404151+Vahom@users.noreply.github.com>
2020-06-11 01:53:19 -0500

    Update proyecto.py
    Se implementaron funciones para transcribir cadenas de ADN a ARN o a su "inverso", se comenzó a implementar una función para transcribir el ARN en proteínas

--- proyecto.py
+++ proyecto.py
@@ -1,16 +1,65 @@
-a= input ('escriba aqui la cadena de ADN')
+#dna=open("unacadena.txt","r")
+#dna=dna.read()
+
+dna= input ('escriba aqui la cadena de ADN')
 dic_aminos={'GGG':'Gly','GGA':'Gly','GGU':'Gly','GGC':'Gly','GAG':'Glu','GAA':'Glu','GAU':'Asp','GAC':'Asp','GUG':'Val','GUA':'Val','GUU':'Val','GUC':'Val','GCG':'Ala','GCA':'Ala','GCU':'Ala','GCC':'Ala',
 'AGG':'Arg','AGA':'Arg','AGU':'Ser','AGC':'Ser','AAG':'Lys','AAA':'Lys','AAU':'Asn','AAC':'Asn','AUG':'Met','AUA':'Iso','AUU':'Iso','AUC':'Iso','ACG':'Thr','ACA':'Thr','ACU':'Thr','ACC':'Thr',
 'UGG':'Try','UGA':'STOP','UGU':'Cys','UGC':'Cys','UAG':'STOP','UAA':'STOP','UAU':'Tyr','UAC':'Tyr','UUG':'Leu','UUA':'Leu','UUU':'Phe','UUC':'Phe','UCG':'Ser','UCA':'Ser','UCU':'Ser','UCC':'Ser',
 'CGG':'Arg','CGA':'Arg','CGU':'Arg','CGC':'Arg','CAG':'Gln','CAA':'Gln','CAU':'His','CAC':'His','CUG':'Leu','CUA':'Leu','CUU':'Leu','CUC':'Leu','CCG':'Pro','CCA':'Pro','CCU':'Pro','CCC':'Pro'}
-a=str(a)
-count=0
-amino=None
-for i in a:
-    #print (i)
-    while count<=3:
-        amino=amino+i
-        count=count+1
-        if count==3:
+dna=dna.upper()
+for i in dna:
+    if i != "A" and i != "G" and i != "T" and i != "C":
+            dna=dna.replace(i,"")
+#print(dna)
+ 
+def RNA (cadena):
+    for i in dna:
+        if i==T:
+            rna=dna.replace("T","U")
+    return rna
+   
+
+
+def INVERSO(cadena):
+    adn=[]
+    for i in cadena:
+        if i =="A":
+            adn.append("T")
+        if i=="T":
+            adn.append("A")
+        if i=="C":
+            adn.append("G")
+        if i=="G":
+            adn.append("C")
+    a=print( "".join(x for x in adn))
+    return a
+
+
+def Transcripicion(cadena):
+    rna=[]
+    for i in cadena:
+        if i =="A":
+            rna.append("U")
+        if i=="T":
+            rna.append("A")
+        if i=="C":
+            rna.append("G")
+        if i=="G":
+            rna.append("C")
+    print( "".join(x for x in rna))
+    
+    
+def Proteinas(cadena):
+    rna=RNA(dna)
+    count=0
+    amino=None
+    print(a)
+    for i in a:
+        print (i)
+        while count<=3:
+            amino=amino+i
+            count=count+1
+            if count==3:
+                None
 
 
