54bb1c0efe83a1af744cf20b090e9e0291f60d37
Steven10P <66403852+Steven10P@users.noreply.github.com>
2020-06-20 22:43:38 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -10,7 +10,7 @@ def lector(archivo):
     t=t.replace(" ", "")
     return t
 
-cadena=lector("AC005021.txt")
+cadena=lector(input ('archivo'))
 
 
 dna= input ('escriba aqui la cadena de ADN')
@@ -78,20 +78,27 @@ def Transcripcion(cadena):
     
 def traductor(seq):
     proteina = ""
-    table = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    table = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':' M', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
     'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K','AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
     'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L','CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
     'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
     'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V','GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
     'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
     'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S','TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':' _ ', 'TAG':' _ ', 'TGC':'C', 'TGT':'C', 'TGA':' _ ', 'TGG':'W',}
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',}
       
     if len(seq)%3 == 0:
         for i in range(0, len(seq), 3):
             codon = seq[i : i+3]
             proteina += table[codon]
+    else:
+        print("cadena incompleta se elimina", len(seq)%3,"nucleotidos")
+        for i in range(0, len(seq)-len(seq)%3, 3):
+            codon = seq[i : i+3]
+            proteina += table[codon]
+        
     return proteina
+
 
 print(traductor(cadena))
 
